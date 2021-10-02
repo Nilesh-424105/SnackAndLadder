a8dec0256e6ef846183d249f504b9a0fc9e0328d
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-02 16:51:32 +0530

    ADDED: Repeat_Till_The_Player_Reaches_Winning_Position

--- Snack_And_Ladder.java
+++ Snack_And_Ladder.java
@@ -2,38 +2,44 @@ package com.bridgelabz;
 
 public class Snack_And_Ladder {
 	public static void main(String[] args) {
-		System.out.println("Player Check option: ");
+		System.out.println("Player Winning Position is: ");
 
-		// Declared Constants and Assigning variables
+		// Declared Constant and Assigning variables
 		int NO_PLAY = 0;
 		int is_Ladder = 1;
 		int is_Snake = 2;
-		int position = 0;
+		int position = 0; // Initialize Variable
 
-		// to roll a dice
-		int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
-		System.out.println("diceNumber is : " + diceNumber);
+		while (position < 100) {
 
-		// to check option
-		int option = (int) Math.floor(Math.random() * 10) % 3;
-		System.out.println("player option is : " + option);
+			// to roll a dice
+			int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
+			System.out.println("diceNumber is : " + diceNumber);
 
-		switch (option) {
-		case 1:
-			position = diceNumber + position;
-			System.out.println("Ladder");
-			System.out.println("position for the player after the ladder is : " + position);
-			break;
+			// check option after getting a diceNumber
+			int option = (int) Math.floor(Math.random() * 10) % 3;
+			System.out.println("selected player option : " + option);
 
-		case 2:
-			position = position - diceNumber;
-			System.out.println("Snack");
-			System.out.println("position for the player after the Snake is : " + position);
-			break;
+			switch (option) {
+			case 1:
+				position = diceNumber + position;
+				System.out.println("Ladder");
+				System.out.println("position for the player after the ladder is : " + position);
+				break;
 
-		default:
-			System.out.println("No play");
+			case 2:
+				position = position - diceNumber;
+				System.out.println("Snack");
+				if (position < 0) {
+					position = 0;
+					System.out.println("Player restart from zero: ");
+				}
+				System.out.println("position for the player after the Snake is : " + position);
+				break;
 
+			default:
+				System.out.println("No play");
+			}
 		}
 	}
 
