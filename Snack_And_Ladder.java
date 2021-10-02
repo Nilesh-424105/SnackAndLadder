7cf75dc5a8bef52e9dd402978cf65532d89a3c28
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-02 16:22:56 +0530

    ADDED: Welcome Message

---
+++ Snack_And_Ladder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class Snack_And_Ladder {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snack and Ladder Computation Problem");
+	}
+
+}
