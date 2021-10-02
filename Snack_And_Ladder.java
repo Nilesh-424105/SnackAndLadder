5f1208ce34e81607959c3f582767da00978c3550
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-02 17:00:04 +0530

    ADDED: Player_Exact_Winning_Position

--- Snack_And_Ladder.java
+++ Snack_And_Ladder.java
@@ -2,28 +2,34 @@ package com.bridgelabz;
 
 public class Snack_And_Ladder {
 	public static void main(String[] args) {
-		System.out.println("Player Winning Position is: ");
+		System.out.println("Player Exact Winning Position is: ");
 
-		// Declared Constant and Assigning variables
+		// Declared Constants and Assigning variables
 		int NO_PLAY = 0;
 		int is_Ladder = 1;
 		int is_Snake = 2;
-		int position = 0; // Initialize Variable
+		int WINNING_POSITION = 100;
+		int position = 0; // Initializing Variable
 
-		while (position < 100) {
+		// while loop use to execute Condition
+		while (position < WINNING_POSITION) {
 
 			// to roll a dice
 			int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
 			System.out.println("diceNumber is : " + diceNumber);
 
 			// check option after getting a diceNumber
-			int option = (int) Math.floor(Math.random() * 10) % 3;
+			int option = (int) Math.floor(Math.random() * 10) % 2;
 			System.out.println("selected player option : " + option);
 
 			switch (option) {
 			case 1:
 				position = diceNumber + position;
 				System.out.println("Ladder");
+				if (position > 100) {
+					position = 100;
+					position = position - diceNumber;
+				}
 				System.out.println("position for the player after the ladder is : " + position);
 				break;
 
@@ -40,6 +46,7 @@ public class Snack_And_Ladder {
 			default:
 				System.out.println("No play");
 			}
+
 		}
 	}
 
